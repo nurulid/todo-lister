679690dd2c61f4060d11afa1be7faef93463593a
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-05-14 17:06:36 +0700

    improve todo input and button behavior

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -7,6 +7,16 @@ export const TodoInput = () => {
   const router = useRouter();
   const [todo, setTodo] = useState("");
   const [priority, setPriority] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+  const handleInputChange = (e) => {
+    setTodo(e.target.value);
+    if (e.target.value.length > 0) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  };
 
   async function createTodo() {
     const res = await fetch("https://v1.appbackend.io/v1/rows/frvryJAmRnM1", {
@@ -31,13 +41,16 @@ export const TodoInput = () => {
   return (
     <div className="mb-5 todo-input">
       <textarea
-        className="border-b border-[#162B5A] w-full bg-[#EEEED0] mb-3 outline-none" rows={5}
+        className="border-b border-[#162B5A] w-full bg-[#EEEED0] mb-3 outline-none"
+        rows={5}
         value={todo}
-        onChange={(e) => setTodo(e.target.value)}
+        onChange={handleInputChange}
         placeholder="Type new todo here..."
       />
       <div className="mb-4 py-1 px-4 text-sm border border-[#162B5A] hover:bg-yellow-100 rounded-full inline-block">
-        <label htmlFor="priority" className="mr-2 cursor-pointer">High priority</label>
+        <label htmlFor="priority" className="mr-2 cursor-pointer">
+          High priority
+        </label>
         <input
           id="priority"
           type="checkbox"
@@ -46,7 +59,11 @@ export const TodoInput = () => {
           className="default:ring-2 cursor-pointer"
         />
       </div>
-      <button className="bg-[#162B5A] hover:bg-[#1d3c7e] text-[#dbe3f5] w-full p-3 rounded-full" onClick={createTodo}>
+      <button
+        className="bg-[#162B5A] hover:bg-[#1d3c7e] text-[#dbe3f5] w-full p-3 rounded-full"
+        onClick={createTodo}
+        disabled={isButtonDisabled}
+      >
         Add Todo
       </button>
     </div>
